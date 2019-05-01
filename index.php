6cd04ea5d93aa71fa42e208bc7b92da6f6907a9a
Frezi2005 <kamil.wan05@gmail.com>
2019-05-01 19:14:41 +0200

    Dodano nowy wygląd całego biletu

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<html>
+    <head>
+        <link href="style.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+    </head>
+</html>
+
 <?php
     // require the Faker autoloader
     require_once 'vendor/fzaninotto/faker/src/autoload.php';
@@ -7,21 +14,56 @@
     $faker = Faker\Factory::create();
     $date = new DateTime('now');
     $now = $date->format('Y-m-d H:i:s');
-    $wylot = $faker->date;
-    //$przylot = $wylot + 6;
+    $departure = $faker->date;
+    //$arrival = $departure + 6;
+    $price = rand(100, 4000);
+    //$flightTime = $faker->dateTimeBetween($startDate = '$departure', $endDate = '$arrival', $timezone = null);
 
-    function planeTicketGenerator($faker, $now) {
+    function planeTicketGenerator($faker, $now, $price) {
 echo <<<END
-        $now
-        <br />
-        From: $faker->country, $faker->city To: $faker->country, $faker->city 
-        <br />
-        Wylot: $faker->date Przylot: $faker->date
-
-
+    <table>
+        <tr style="background-color: #a8d8ff">
+            <td colspan="3" style="text-align:left;">TICKETS IN REAL PRICE</td>
+        </tr>
+        <tr>
+            <td style="width:200px;"></td>
+            <td><img src="BoeingLogo.png" alt="BoeingLogo" style="width:200px;"/></td>
+            <td><img src="AirPlaneLogo.png" alt="AirPlaneLogo" style="width:200px;"/></td>
+        </tr>
+        <tr style="height:50px;background-color: #93cfff;">
+            <td>From: $faker->city</td>
+            <td>To: $faker->city</td>
+            <td></td>
+        </tr>
+        <tr style="background-color: #93cfff;">
+            <td>XXX</td>
+            <td>XXX</td>
+            <td>XXX</td>
+        </tr>
+        <tr style="background-color: #93cfff;">
+            <td colspan="2">Departure: $faker->date</td>
+            <td>Price: $price PLN</td>
+        </tr>
+        <tr>
+            <td>Flight with the airline: Ryanair</td>
+            <td colspan="2">Passenger surname: $faker->firstName $faker->lastName</td>
+        </tr>
+    </table>
 END;
     }
 
-    planeTicketGenerator($faker, $now);
+    planeTicketGenerator($faker, $now, $price);
 
+    // $now
+    //     <br />
+    //     From: $faker->city To: $faker->city 
+    //     <br />
+    //     Departure: $faker->date Arrival: $faker->date
+    //     <br />
+    //     Name: $faker->firstName Surname: $faker->lastName
+    //     <br />
+    //     Adress: $faker->streetAddress
+    //     <br />
+    //     Price: $price
 ?>
+
